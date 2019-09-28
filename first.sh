45db7ae7f8e626cc52c993714aedc2749e6f90b7
Rajiv <rjhaveri06@outlook.com>
2019-09-28 15:01:15 -0400

    after branching and deleting branches

--- first.sh
+++ first.sh
@@ -3,9 +3,6 @@
 echo "hello world!"
 echo "hello world2"
 echo "testing out stash!!"
-<<<<<<< HEAD
 echo "ths echo was done in branchTesting"
-echo "ths echo was done in branchTesting after hotfix branch was created and merged with master and deleted "
-=======
 echo "this line is for hotfix branch"
->>>>>>> master
+echo "ths echo was done in branchTesting after hotfix branch was created and merged with master and deleted "
