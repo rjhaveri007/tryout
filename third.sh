0baff6d5d1d76ad8acae2e2d9b996a0cf8a62723
Rajiv <rjhaveri06@outlook.com>
2020-08-15 22:06:46 -0400

    My thrd file

---
+++ third.sh
@@ -0,0 +1,9 @@
+#!/bin/ksh
+
+echo "hello world!"
+echo "hello world2"
+echo "testing out stash!!"
+echo "ths echo was done in branchTesting"
+echo "this line is for hotfix branch"
+echo "ths echo was done in branchTesting after hotfix branch was created and merged with master and deleted "
+echo "ths echo was done in testing"
